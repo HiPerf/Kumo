b5a7348712a117905fd282276d95912a8ada5070
gpascualg <gpascualg93@gmail.com>
2021-06-24 19:34:40 +0200

    Fix C++ std::get invocation

--- lib/v2/lang/kaminari_cpp.py
+++ lib/v2/lang/kaminari_cpp.py
@@ -543,9 +543,7 @@ class LangGenerator(generator.Generator):
             gen.Variable('Args&&...', 'allocator_args')
         ], visibility=gen.Visibility.PUBLIC, template=gen.Statement('template <typename... Args>', ending=''))
         
-        for i, queue_name in enumerate(self.queues.keys()):
-            if not self.queue_usage[queue_name]:
-                continue
+        for i, queue_name in enumerate(x for x in self.queues.keys() if not self.queue_usage[x]):
             constructor.initializers.append(gen.Statement(f'_{queue_name}(resend_threshold, std::get<{i}>(std::forward_as_tuple(allocator_args...)))', ending=''))
 
         queues.methods.append(constructor)
